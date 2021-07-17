3d410439cf239d9f0536976bcfdcd47878dba74f
Eva813 <as45986@gmail.com>
2021-07-17 17:18:28 +0800

    modify home.vue ,add nav part

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<header>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="@/assets/img/original.png" alt="">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">🐷 開鍋祭，開鍋囉~</a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            所有商品
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">POPOLA營養師專欄</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">會員福利</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">《防疫期間物流變更公告》</a>
+        </li>
+        
+      </ul>
+      <div class="d-flex icon-group">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </div>
+    </div>
   </div>
+</nav>
+</header>
 </template>
 
 <script>

2f57faf1d0f15fb8ca5e41208f5e40f6a3ac97a8
Eva813 <as45986@gmail.com>
2021-07-18 21:52:31 +0800

    add ProductCard.vue ,use BS5 to add card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+
+<style>
+</style>
+<template>
+  <div class="row">
+    <div class="col-4">
+      <div class="card product-card" style="width: 18rem">
+        <img
+          src="@/assets/img/food/food1-1.png"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      food: [
+        {
+          title: "酵素旅行包(20入/盒)",
+          price: 1680,
+          src: require("@/assets/img/food/food1-1.png"),
+        },
+      ],
+    };
+  },
+};
+</script>

a7d7e64d34ee29a9ea18453d2f3d11129334dcbc
Eva813 <as45986@gmail.com>
2021-07-18 16:25:56 +0800

    modify slide button ,add img

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -29,16 +29,34 @@
         data-bs-slide-to="2"
         aria-label="Slide 3"
       ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="3"
+        aria-label="Slide 4"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="4"
+        aria-label="Slide 5"
+      ></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="@/assets/img/logo1.png" class="d-block w-100" alt="..." />
+        <img src="@/assets/img/banner1.png" class="d-block w-100" alt="..." />
       </div>
       <div class="carousel-item">
-        <img src="@/assets/img/logo1.png" class="d-block w-100" alt="..." />
+        <img src="@/assets/img/banner2.png" class="d-block w-100" alt="..." />
       </div>
       <div class="carousel-item">
-        <img src="@/assets/img/logo1.png" class="d-block w-100" alt="..." />
+        <img src="@/assets/img/banner3.png" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="@/assets/img/banner4.png" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="@/assets/img/banner5.png" class="d-block w-100" alt="..." />
       </div>
     </div>
     <button

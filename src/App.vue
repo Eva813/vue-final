506d2ed056ef888628434c0aec5b3ab77ce2aaca
Eva813 <as45986@gmail.com>
2021-07-19 22:45:57 +0800

    modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <!-- <router-link to="/">Home</router-link> | -->
     <!-- <router-link to="/about">About</router-link> -->
+    <!-- <Home></Home> -->
   </div>
-
   <router-view />
 </template>
 
@@ -11,3 +11,11 @@
 @import "~@/assets/all.scss";
 // @import "font-awesome/css/font-awesome.min.css";
 </style>
+<script>
+// import Home from "@/views/Home.vue";
+// export default {
+//   components: {
+//     Home,
+//   },
+// };
+</script>

c00391375163c718a25ceab1bc35df1d1855366b
Eva813 <as45986@gmail.com>
2021-07-20 23:10:55 +0800

    modify style of food-card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,19 +1,66 @@
 
-<style>
+<style lang="scss" scoped>
+//替圖片增加淺黑色https://blog.csdn.net/fxjzzyo/article/details/79263324
+.card {
+  border: 0;
+  .card-top {
+    position: relative;
+    background-color: #000;
+    transition: all 0.6s ease-in-out;
+    &:hover .card-img-top {
+      -webkit-mask: -webkit-linear-gradient(
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.5),
+        rgba(0, 0, 0, 0.6),
+        rgba(0, 0, 0, 0.5)
+      );
+    }
+    &:hover .card-btn {
+      // display: block;
+      z-index: 999;
+    }
+  }
+
+  .card-img-top {
+    height: 260px;
+    width: 260px;
+    width: 100%;
+    border-radius: 3px;
+  }
+  &-btn {
+    position: absolute;
+    // display: none;
+    width: 90%;
+    height: 45px;
+    top: 88%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transition: all 0.3s ease;
+    background-color: #fff;
+    color: #000;
+    z-index: -1;
+    &:hover {
+      background-color: #faaf48;
+    }
+  }
+}
 </style>
 <template>
-  <div class="container d-flex">
+  <div class="container d-flex mb-3">
     <div class="row row-cols-md-4">
       <div class="col gap-4" v-for="item in ifood" :key="item.title">
         <div class="card product-card">
-          <img :src="item.src" class="card-img-top" alt="..." />
+          <div class="card-top">
+            <a class="card-img-link" href="#">
+              <img :src="item.src" class="card-img-top" alt="..." />
+            </a>
+            <a href="#" class="btn card-btn">Go somewhere</a>
+          </div>
           <div class="card-body">
-            <h5 class="card-title">{{ item.title }}</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+            <h5 class="card-title text-center fw-normal">{{ item.title }}</h5>
+            <p class="card-text text-center fw-bold">
+              <span>NT$</span> {{ item.price }}
             </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
         </div>
       </div>

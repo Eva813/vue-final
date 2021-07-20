aa8ceac0faacd91ba4cfb4c29f48fb0d8c98b5cb
Eva813 <as45986@gmail.com>
2021-07-20 20:29:01 +0800

    change v-for position and modify flex,column

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,25 +2,19 @@
 <style>
 </style>
 <template>
-  <div class="row">
-    <div class="col d-flex gap-4">
-      <div
-        class="card product-card justify-content-around"
-        v-for="item in inFood"
-        :key="item.title"
-      >
-        <img
-          src="@/assets/img/food/food1-1.png"
-          class="card-img-top"
-          alt="..."
-        />
-        <div class="card-body">
-          <h5 class="card-title">{{ item.title }}</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="container d-flex">
+    <div class="row row-cols-md-4">
+      <div class="col gap-4" v-for="item in ifood" :key="item.title">
+        <div class="card product-card">
+          <img :src="item.src" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ item.title }}</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
       </div>
     </div>
@@ -29,7 +23,7 @@
 
 <script>
 export default {
-  props: ["inFood"],
+  props: ["ifood"],
   data() {
     return {};
   },

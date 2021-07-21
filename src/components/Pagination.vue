bac34d1af7609066ae061f59fcfcfea24660f0e1
Eva813 <as45986@gmail.com>
2021-07-21 21:11:42 +0800

    modify link

---
+++ src/components/Pagination.vue
@@ -0,0 +1,38 @@
+<style lang="scss" scoped>
+</style>
+
+
+<template>
+  <div class="container">
+    <div class="row">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a class="page__link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page__link" href="#">1</a>
+          </li>
+          <li class="page-item">
+            <a class="page__link" href="#">2</a>
+          </li>
+          <li class="page-item">
+            <a class="page__link" href="#">3</a>
+          </li>
+          <li class="page-item">
+            <a class="page__link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+

1cf8071a7f9f44f683eb940c338898b0388619bc
Eva813 <as45986@gmail.com>
2021-07-21 21:13:40 +0800

    add pagination

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,48 +1,7 @@
 
 <style lang="scss" scoped>
-//替圖片增加淺黑色https://blog.csdn.net/fxjzzyo/article/details/79263324
-.card {
+.product-card {
   border: 0;
-  .card-top {
-    position: relative;
-    background-color: #000;
-    transition: all 0.6s ease-in-out;
-    &:hover .card-img-top {
-      -webkit-mask: -webkit-linear-gradient(
-        rgba(0, 0, 0, 0.5),
-        rgba(0, 0, 0, 0.5),
-        rgba(0, 0, 0, 0.6),
-        rgba(0, 0, 0, 0.5)
-      );
-    }
-    &:hover .card-btn {
-      // display: block;
-      z-index: 999;
-    }
-  }
-
-  .card-img-top {
-    height: 260px;
-    width: 260px;
-    width: 100%;
-    border-radius: 3px;
-  }
-  &-btn {
-    position: absolute;
-    // display: none;
-    width: 90%;
-    height: 45px;
-    top: 88%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    transition: all 0.3s ease;
-    background-color: #fff;
-    color: #000;
-    z-index: -1;
-    &:hover {
-      background-color: #faaf48;
-    }
-  }
 }
 </style>
 <template>

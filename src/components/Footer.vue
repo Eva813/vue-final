f378f4ac4ecb7bed9bc99b8ca5efafe8eb8fdf37
Eva813 <as45986@gmail.com>
2021-07-23 15:16:00 +0800

    add component Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,69 @@
+
+<style lang="scss" scoped >
+.icon-list {
+  img {
+    height: 44px;
+    width: auto;
+  }
+}
+</style>
+<template>
+  <div class="container border-top justify-content-between">
+    <div class="row">
+      <div class="col-4">
+        <h3>NEED HELP</h3>
+        <ul>
+          <li>(02)2297-0588</li>
+          <li>線上客服</li>
+          <li>-官網訂單訊息</li>
+          <li>-POPOLA Facebook 粉絲專頁</li>
+          <li>Mon-Fri(例假日除外) 9:30AM-6:00PM</li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <h3>ABOUT US</h3>
+        <ul>
+          <li><a href="#" target="_blank"> 購買方法</a></li>
+          <li><a href="#" target="_blank">退換貨政策</a></li>
+          <li><a href="#" target="_blank">常見Q &amp; A</a></li>
+          <li><a href="#" target="_blank">SGS認證</a></li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <h3>FOLLOW</h3>
+        <ul class="icon-list d-flex">
+          <li>
+            <a href="#"><img src="https://i.imgur.com/MekLQay.png" alt="" /></a>
+          </li>
+          <li>
+            <a href="#"
+              ><img src="https://imgur.com/6QzKhtx" alt="" srcset=""
+            /></a>
+          </li>
+          <li>
+            <a href="#"><img src="https://i.imgur.com/UrpZFdz.png" alt="" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ul class="pay-img">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="copyright">POPOLA版權所有 Copyright &copy; 2017</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

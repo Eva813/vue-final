890df9bcd51ca2ca81456fb51882698fada75be3
Eva813 <as45986@gmail.com>
2021-07-24 19:01:24 +0800

    modify fontawsome,  setting for swiper and agile

--- src/main.js
+++ src/main.js
@@ -5,19 +5,34 @@ import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faShoppingCart, faUser, faComments, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart, faUser, faComments, faSearch, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+import VueAgile from 'vue-agile'
+
 const app = createApp(App);
-library.add(faUserSecret, faShoppingCart, faUser, faComments, faSearch);
-
-
+library.add(faUserSecret, faShoppingCart, faUser, faComments, faSearch, faChevronRight, faChevronLeft);
 
 app.component('font-awesome-icon', FontAwesomeIcon);
-
+app.use(VueAgile)
 
 app.use(store);
 app.use(router);
 app.mount('#app');
+
+
+
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import swiper, { Navigation, Pagination, Autoplay } from 'swiper'
+// //上面這行官方文件沒寫到。
+// // import style (>= Swiper 6.x)
+// // import style (<= Swiper 5.x)
+// import 'swiper/dist/css/swiper.css'
+//
+//
+// app.use(VueAwesomeSwiper, /* { default options with global component } */)
+// swiper.use([Navigation, Pagination, Autoplay])
+//上面這行官方文件也沒寫到

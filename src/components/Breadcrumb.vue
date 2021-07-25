e6567f34812307db833f958958bc40e51e68bec0
Eva813 <as45986@gmail.com>
2021-07-25 11:27:26 +0800

    modify breadcrumb

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,28 @@
+<style lang="scss" scoped>
+@import "~@/assets/all.scss";
+.inner-bread {
+  background-color: #f7f7f7;
+  padding: 10px 0;
+  font-size: 13px;
+  color: #999;
+  $breadcrumb-divider: quote(">");
+}
+</style>
+
+<template>
+  <div class="inner-bread">
+    <div class="container">
+      <nav style="--bs-breadcrumb-divider: '>'" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">全部商品</a></li>
+          <li class="breadcrumb-item active" aria-current="page">美食生活</li>
+        </ol>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
--- src/views/FoodPage.vue
+++ src/views/FoodPage.vue
@@ -1,16 +1,53 @@
 <template>
   <header>
     <Navbar></Navbar>
+
+    <Breadcrumb></Breadcrumb>
   </header>
+  <div class="aside-menu">
+    <ul class="catagory">
+      <li>
+        <a href="#">🐷 開鍋祭，開鍋囉~</a>
+      </li>
+      <li>
+        <a href="#">美食生活</a>
+        <ul>
+          <li><a href="#">冷凍食品</a></li>
+          <li><a href="#">POPOLA BAKE</a></li>
+          <li><a href="#">廚房用品</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">美妝保養</a>
+        <ul>
+          <li><a href="#">保養系列</a></li>
+          <li><a href="#">美妝系列</a></li>
+          <li><a href="#">日程生活</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">毛孩愛用</a>
+        <ul>
+          <li><a href="#">狗狗食品</a></li>
+          <li><a href="#">狗狗用品</a></li>
+          <li><a href="#">老犬照護</a></li>
+          <li><a href="#">貓貓食品</a></li>
+          <li><a href="#">貓貓用品</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Breadcrumb from "@/components/Breadcrumb.vue";
 export default {
   name: "FoodPage",
   components: {
     Navbar,
+    Breadcrumb,
   },
 };
 </script>

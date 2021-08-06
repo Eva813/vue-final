5ea66cf76774388c6305e3dd0b3a5acef9af867c
Eva813 <as45986@gmail.com>
2021-08-06 11:16:33 +0800

    add new view , shopping cart

---
+++ src/views/ShoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "ShoppingCart",
+components: {
+  Navbar
+}
+
+}
+</script>
+
+<style>
+
+</style>

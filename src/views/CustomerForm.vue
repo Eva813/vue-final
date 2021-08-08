055adf6467a90a962d5645552746fecc42ad8f5f
Eva813 <as45986@gmail.com>
2021-08-08 10:47:40 +0800

    init the next step of check out (customer form)

---
+++ src/views/CustomerForm.vue
@@ -0,0 +1,31 @@
+<style lang="scss" scoped>
+@import "~@/assets/all.scss";
+</style>
+
+<template>
+  <header class="mb-4">
+    <Navbar></Navbar>
+  </header>
+  <section class="mb-4">
+    <div class="container">
+      <div class="row">
+        <ul class="time-line">
+          <li class="active-tl">購物車</li>
+          <li>填寫資料</li>
+          <li>訂單確認</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "CustomerForm",
+  components: {
+    Navbar,
+  },
+};
+</script>
+

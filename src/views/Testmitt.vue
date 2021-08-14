880d1df4387a09a2e0e7dda347290c3de8051abd
Eva813 <as45986@gmail.com>
2021-08-14 13:50:00 +0800

    test mitt

--- src/views/Testmitt.vue
+++ src/views/Testmitt.vue
@@ -2,14 +2,12 @@
   <!-- <component-a></component-a> -->
   {{ text }} <button type="button" @click="pushData">發送至另一個元件</button>
   <hr />
-  <component-b></component-b>
+  <!-- <component-b></component-b> -->
 </template>
 
 <script>
 import componentB from "@/components/Component-b.vue";
-import mitt from "mitt";
 
-const emitter = mitt();
 export default {
   components: {},
   data() {

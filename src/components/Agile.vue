a1f3e24b772206e88220ce290b6d734bd3d16700
Eva813 <as45986@gmail.com>
2021-08-15 14:48:42 +0800

    modify img width

--- src/components/Agile.vue
+++ src/components/Agile.vue
@@ -1,3 +1,86 @@
+<style lang="scss" scoped>
+@import "~@/assets/all.scss";
+.main {
+  margin-bottom: 20px;
+}
+
+.thumbnails {
+  margin: 0 -5px;
+  width: calc(100% + 10px);
+}
+
+.agile__nav-button {
+  background: transparent;
+  border: none;
+  color: #ccc;
+  cursor: pointer;
+  font-size: 24px;
+  transition-duration: 0.3s;
+}
+.thumbnails .agile__nav-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.thumbnails .agile__nav-button--prev {
+  left: -45px;
+}
+.thumbnails .agile__nav-button--next {
+  right: -45px;
+}
+.agile__nav-button:hover {
+  color: #888;
+}
+.agile__dot {
+  margin: 0 10px;
+}
+.agile__dot button {
+  background-color: #eee;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  display: block;
+  height: 10px;
+  font-size: 0;
+  line-height: 0;
+  margin: 0;
+  padding: 0;
+  transition-duration: 0.3s;
+  width: 10px;
+}
+.agile__dot--current button,
+.agile__dot:hover button {
+  background-color: #888;
+}
+
+.slide {
+  align-items: center;
+  box-sizing: border-box;
+  color: #fff;
+  display: flex;
+  max-height: 540px;
+  justify-content: center;
+}
+.slide--thumbniail {
+  cursor: pointer;
+  height: 100px;
+  padding: 0 5px;
+  transition: opacity 0.3s;
+}
+.slide--thumbniail:hover {
+  opacity: 0.75;
+}
+.slide img {
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  -o-object-position: center;
+  object-position: center;
+  width: 100%;
+}
+</style>
+
+
 <template>
   <agile class="main" ref="main" :options="options1" :as-nav-for="asNavFor1">
     <div
@@ -104,84 +187,3 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-@import "~@/assets/all.scss";
-.main {
-  margin-bottom: 20px;
-}
-
-.thumbnails {
-  margin: 0 -5px;
-  width: calc(100% + 10px);
-}
-
-.agile__nav-button {
-  background: transparent;
-  border: none;
-  color: #ccc;
-  cursor: pointer;
-  font-size: 24px;
-  transition-duration: 0.3s;
-}
-.thumbnails .agile__nav-button {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-}
-.thumbnails .agile__nav-button--prev {
-  left: -45px;
-}
-.thumbnails .agile__nav-button--next {
-  right: -45px;
-}
-.agile__nav-button:hover {
-  color: #888;
-}
-.agile__dot {
-  margin: 0 10px;
-}
-.agile__dot button {
-  background-color: #eee;
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
-  display: block;
-  height: 10px;
-  font-size: 0;
-  line-height: 0;
-  margin: 0;
-  padding: 0;
-  transition-duration: 0.3s;
-  width: 10px;
-}
-.agile__dot--current button,
-.agile__dot:hover button {
-  background-color: #888;
-}
-
-.slide {
-  align-items: center;
-  box-sizing: border-box;
-  color: #fff;
-  display: flex;
-  height: 540px;
-  justify-content: center;
-}
-.slide--thumbniail {
-  cursor: pointer;
-  height: 100px;
-  padding: 0 5px;
-  transition: opacity 0.3s;
-}
-.slide--thumbniail:hover {
-  opacity: 0.75;
-}
-.slide img {
-  height: 100%;
-  -o-object-fit: cover;
-  object-fit: cover;
-  -o-object-position: center;
-  object-position: center;
-  width: 100%;
-}
-</style>

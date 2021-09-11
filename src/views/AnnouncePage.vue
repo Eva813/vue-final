d9eeec3e866c7143a02f68629f406559e9d63cac
Eva813 <as45986@gmail.com>
2021-09-11 10:12:20 +0800

    add AnnouncePage

---
+++ src/views/AnnouncePage.vue
@@ -0,0 +1,17 @@
+<style lang="scss" scoped >
+@import "~@/assets/all.scss";
+</style>
+<template></template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  name: "AnnouncePage",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+

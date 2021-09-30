deeef19252fa6c6f02e10d4cd27dfec2cdfc0eb9
Eva813 <as45986@gmail.com>
2021-09-30 17:11:28 +0800

    add emit to get data from cartPanel

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -13,7 +13,12 @@
             <router-link class="card-img-link" to="/foodproduct">
               <img :src="item.src" class="card-img-top" alt="product-img" />
             </router-link>
-            <a href="#" class="btn card-btn">加入購物車</a>
+            <div
+              class="btn card-btn"
+              @click="addToCart(item), triggerPanel(), pushData()"
+            >
+              加入購物車
+            </div>
           </div>
           <div class="card-body">
             <h5 class="card-title text-center fw-normal">{{ item.title }}</h5>
@@ -32,7 +37,108 @@
 export default {
   props: ["ifood"],
   data() {
-    return {};
+    return {
+      spanNumbers: 0,
+      shoppingCart: [],
+      showPanel: true,
+    };
+  },
+  methods: {
+    addToCart(foods) {
+      this.cartNumbers(foods);
+      this.totalPrice(foods);
+    },
+    cartNumbers(theFood) {
+      let productNumbers = localStorage.getItem("cartNumbers");
+      //取得的productNumbers 型別是string，所以要轉為數字
+      productNumbers = parseInt(productNumbers);
+      //要判斷我的localStorage是否已經有資料存在
+      if (productNumbers) {
+        localStorage.setItem("cartNumbers", productNumbers + 1);
+        this.spanNumbers = productNumbers + 1;
+      } else {
+        localStorage.setItem("cartNumbers", 1);
+        this.spanNumbers = 1;
+      }
+      this.setItems(theFood);
+    },
+    setItems(theFood) {
+      const cacheCarID = []; // 暫存 ID 放置處
+      // 一開始先將 carData 中的 ID 全部撈出來
+      this.shoppingCart.forEach((item) => {
+        cacheCarID.push(item.productId);
+      });
+      // 接下來使用 indexOf 尋找傳進來的參數 ID 是否有在該陣列中
+      // 若不存在則會回傳 -1 並加入到陣列儲存在 localStorage
+      // 若存在則會回傳 0 並改走 else
+      if (cacheCarID.indexOf(theFood.productId) === -1) {
+        const cartContent = {
+          productId: theFood.productId, // 產品 ID
+          inCart: 1, // 產品數量，預設一筆
+          title: theFood.title, // 產品標題
+          price: theFood.price, // 產品銷售金額
+          src: theFood.src,
+        };
+        // 將數量推回陣列中
+        this.shoppingCart.push(cartContent);
+        // 重新寫入 localStorage
+        localStorage.setItem(
+          "productsInCart",
+          JSON.stringify(this.shoppingCart)
+        );
+      } else {
+        let cache = {}; // 產品暫存處
+        this.shoppingCart.forEach((item, keys) => {
+          // 只找相同的產品內容
+          if (item.productId === theFood.productId) {
+            let { inCart } = item; // 取出已存在 localStorage 購物車的資料並加數量增加
+            cache = {
+              productId: theFood.productId, // 產品 ID
+              inCart: (inCart += 1), // 產品當前數量，針對數量增加數量
+              title: theFood.title, // 產品標題
+              price: theFood.price, // 產品原始金額
+              src: theFood.src,
+            };
+            // 移除現有 localStorage 購物車的資料，否則 localStorage 會重複加入
+            this.shoppingCart.splice(keys, 1);
+          }
+        });
+        // 將數量已經增加的資料推回陣列中
+        this.shoppingCart.push(cache); // 不建議放在 forEach 內，否則迴圈會重複執行導致變成加二
+        localStorage.setItem(
+          "productsInCart",
+          JSON.stringify(this.shoppingCart)
+        );
+      }
+      //console.log(this.shoppingCart);
+    },
+    totalPrice(theFoodPrice) {
+      let cartPrice = localStorage.getItem("totalPrice");
+      //console.log("the price is", cartPrice);
+
+      if (cartPrice != null) {
+        cartPrice = parseInt(cartPrice);
+        localStorage.setItem("totalPrice", cartPrice + theFoodPrice.price);
+      } else {
+        localStorage.setItem("totalPrice", theFoodPrice.price);
+      }
+    },
+    triggerPanel() {
+      this.showPanel = true;
+      setTimeout(() => (this.showPanel = false), 4000);
+    },
+    pushData() {
+      this.emitter.emit("getData", this.shoppingCart);
+      // this.emitter.emit("go", this.goto());
+      this.emitter.emit("getPanel", this.showPanel);
+      console.log(this.showPanel);
+    },
+    goto() {
+      // var element = this.$refs[refName];
+      // console.log(element);
+      // var top = element.offsetTop;
+      window.scrollTo(0, top);
+    },
   },
 };
 </script>
